d7b3abdc402d9c823b5807c35f6557b47ed390e6
Paul Serby <paul.serby@clock.co.uk>
2013-06-17 16:41:58 +0100

    Conditionals

--- setup.sh
+++ setup.sh
@@ -7,26 +7,45 @@ sudo sh -c 'curl -fsSL https://raw.github.com/isaacs/nave/master/nave.sh > /usr/
 # Install a global node.js
 nave usemain stable
 
-# Download and Install Command Line Tools for Xcode - https://developer.apple.com/downloads/download.action?path=Developer_Tools/command_line_tools_os_x_mountain_lion_for_xcode__april_2013/xcode462_cltools_10_86938259a.dmg
-curl -O https://dl.dropboxusercontent.com/u/4119997/Setup/xcode462_cltools_10_86938259a.dmg && open xcode462_cltools_10_86938259a.dmg
+# Is xcode needed?
+if [ -x `which xcodebuild` ]; then
+    echo Skipping xcode
+else
+  echo Installing Xcode CLI tools
+  # Download and Install Command Line Tools for Xcode - https://developer.apple.com/downloads/download.action?path=Developer_Tools/command_line_tools_os_x_mountain_lion_for_xcode__april_2013/xcode462_cltools_10_86938259a.dmg
+  curl -O https://dl.dropboxusercontent.com/u/4119997/Setup/xcode462_cltools_10_86938259a.dmg && open xcode462_cltools_10_86938259a.dmg
 
-# Install Xcode
-# https://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12
+  # Install Xcode
+  # https://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12
 
-echo "Press enter once Command Line Tools for Xcode are installed"
-read
+  echo "Press enter once Command Line Tools for Xcode are installed"
+  read
+fi
 
-# Install Brew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+# Accept the Xcode licence
+xcodebuild -license
 
-# Install MongoDB
-brew install mongo
+# Is brew needed?
+if [ -x `which brew` ]; then
+  echo Skipping brew
+else
+  echo Installing brew
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
 
-# Launch mongo on startup
-ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
+# Is MongoDB needed?
+if [ -x `which mongo` ]; then
+  echo Skipping MongoDB
+else
+  echo Installing MongoDB
+  brew install mongo
 
-# Start mongo now
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+  # Launch mongo on startup
+  ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
+
+  # Start mongo now
+  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+fi
 
 # Install ZeroMQ
 brew install 0mq
